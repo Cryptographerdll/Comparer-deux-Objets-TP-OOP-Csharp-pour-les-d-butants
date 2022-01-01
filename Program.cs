6ce79a664c9889857832c78c1760f26bfd655385
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2022-01-01 16:44:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Comparaison_de_2_Objets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stagaire s1 = new Stagaire();
+
+            Console.WriteLine("give the s1 informations in below ");
+
+            s1.Num = int.Parse(Console.ReadLine());
+            s1.Nom = Console.ReadLine();
+            s1.Prenom = Console.ReadLine();
+            s1.datenaissance = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Your data is \n");
+
+            s1.Afficher();
+
+            // 2eme Stagaire a comparer 
+
+            Stagaire s2 = new Stagaire();
+
+            s2.Num = int.Parse(Console.ReadLine());
+            s2.Nom = Console.ReadLine();
+            s2.Prenom = Console.ReadLine();
+            s2.datenaissance = Convert.ToDateTime(Console.ReadLine());
+
+            if (s1.ComparerDeuxStagaires(s2) == true)
+                Console.WriteLine(true);
+
+            else
+                Console.WriteLine(false);
+
+
+        }
+    }
+}
---
+++ Stagaire.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparaison_de_2_Objets
+{
+    class Stagaire
+    {
+        private int num;
+        private string nom, prenom;
+        private DateTime dateNaissance;
+
+        // Constructeur par defaut 
+
+        public Stagaire()
+        {
+
+        }
+
+        // Constructeur d'Initialisation 
+        public Stagaire(int num, string nom, string prenom, DateTime dateNaissance)
+        {
+            this.num = num;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateNaissance = dateNaissance;
+        }
+
+        public Stagaire(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        // Getters & Setters 
+
+        public int Num
+        {
+            get
+            {
+                return this.num;
+            }
+
+            set
+            {
+                this.num = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                this.prenom = value;
+            }
+        }
+
+        public DateTime datenaissance
+        {
+            get
+            {
+                return this.dateNaissance;
+            }
+
+            set
+            {
+                this.dateNaissance = value;
+            }
+        }
+
+        // Methods 
+
+        public void Afficher()
+        {
+            Console.WriteLine("hello Guys ");
+            Console.WriteLine("Le num est: " + this.Num);
+            Console.WriteLine("Le nom est: " + this.Nom);
+            Console.WriteLine("Le prenom est: " + this.Prenom);
+            Console.WriteLine("La date de naissance est: " + this.datenaissance);
+        }
+
+        // Comparer deux stagaires 
+
+        public bool ComparerDeuxStagaires (Stagaire s)
+        {
+            if (this.Num == s.Num)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
